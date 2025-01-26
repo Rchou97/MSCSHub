c07fa928fb72b700e67a20325ddd5974edc4b2e3
Richard <93627768+Rchou97@users.noreply.github.com>
2025-01-27 00:02:26 +0100

    Remove files

--- src/app/pipes/faq/faq-categories.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FaqCategoriesPipe } from './faq-categories.pipe';
-
-describe('FaqCategoriesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FaqCategoriesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
